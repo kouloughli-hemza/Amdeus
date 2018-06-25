55d40028eb44c8084a6c673ed0c960a0616f3b09
zaki <shoper_06@outlook.com>
2018-06-25 17:52:41 +0100

    First Commit

---
+++ src/AmadeusProvider.php
@@ -0,0 +1,36 @@
+<?php
+namespace AmadeusDzVoyagezak\Amadeus;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Session;
+
+
+class AmadeusProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if(!file_exists(base_path('config').'/amadeus.php'))
+        {
+          $this->publishes([__DIR__.'/config'=>base_path('config')]);   
+        }
+
+        app()->singleton('amadeus',function(){
+            return new AmadeusDzVoyagezak\Amadeus\Amadeus();
+        });
+       return new Amadeus();
+    }
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+       
+    }
+}
---
+++ src/config/amadeus.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Amadeus DZ Voyage -- Recherche Vols 
+*/
+return [
+	'apikey'=>'Amdeus API KEY ici',
+	'sandbox'=>true,
+	'sandboxlink'=>'https://api.sandbox.amadeus.com/v1.2',
+	'livelink'=>'https://api.live.amadeus.com/v1.2',
+	'timeout'=>30,
+	'RETURNTRANSFER'=>true,
+];
